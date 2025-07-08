bad90a9347eb80ffbcfb55281c6a6dd56741291d
akimkhabibullin <akim.khabibullin@outlook.com>
2025-07-08 19:54:17 +0000

    making login page

--- EcoVoyage_App/app/(tabs)/_layout.tsx
+++ EcoVoyage_App/app/(tabs)/_layout.tsx
@@ -24,30 +24,16 @@ export default function TabLayout() {
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         // Disable the static render of the header on web
         // to prevent a hydration error in React Navigation v6.
-        headerShown: useClientOnlyValue(false, true),
+        headerShown: true,
       }}>
       <Tabs.Screen
-        name="home"
+        name="login"
         options={{
-          title: 'Home',
+          title: 'Login',
           tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
           headerStyle: { backgroundColor: '#97e28f' },
           headerTintColor: '#',
-          headerTitleStyle: { fontWeight: 'bold' },
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="bars"
-                    size={25}
-                    color="#"
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
+          tabBarStyle: { display: 'none' },
         }}
       />
       <Tabs.Screen
---
+++ EcoVoyage_App/app/(tabs)/login.tsx
@@ -0,0 +1,83 @@
+import { StyleSheet, TextInput } from 'react-native';
+import React, { useState } from 'react';
+
+import EditScreenInfo from '@/components/EditScreenInfo';
+import { Text, View } from '@/components/Themed';
+
+export default function TabOneScreen() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome To Eco Voyage!</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="#000" />
+      <View style={styles.grayBox}>
+        <Text style={styles.label}>Email</Text>
+        <TextInput
+          style={styles.input}
+          value={email}
+          onChangeText={setEmail}
+          placeholder="name@example.com"
+          placeholderTextColor="#ccc"
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        <Text style={styles.label}>Password</Text>
+        <TextInput
+          style={styles.input}
+          value={password}
+          onChangeText={setPassword}
+          placeholder="password"
+          placeholderTextColor="#ccc"
+          autoCapitalize="none"
+          secureTextEntry
+        />
+        </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start', 
+    marginTop: 200, 
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+  separator: {
+    marginVertical: 20,
+    height: 2,
+    borderRadius: 100,
+    width: '80%',
+  },
+  grayBox: {
+    width: '80%',
+    height: 200,
+    backgroundColor: '#39a465',
+    borderRadius: 10,
+    marginTop: 10,
+  },
+    label: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginTop: 8,
+    marginLeft: 8,
+    padding: 8,
+  },
+  input: {
+    width: '90%',
+    height: 40,
+    backgroundColor: '#fff',
+    borderRadius: 6,
+    paddingHorizontal: 10,
+    fontSize: 13,
+    marginLeft: 16,
+  },
+});
